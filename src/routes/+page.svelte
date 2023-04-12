c736c7e3a9aca1fc132fbf3bf43fe699e3a0daf1
henrikvilhelmberglund <henrikvilhelmberglund@gmail.com>
2023-04-12 21:58:00 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Game from "./Game.svelte";
+</script>
+
+
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+
+<style>
+  
+</style>
---
+++ src/routes/Game.svelte
@@ -0,0 +1,26 @@
+<script>
+  
+</script>
+
+<div class="game">
+  <div class="info">
+    
+  </div>
+  <div class="grid-container">
+    
+  </div>
+
+</div>
+
+<style>
+  .game {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .info {
+    width: 80vmin;
+  }
+</style>
